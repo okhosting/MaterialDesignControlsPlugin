0044d04c85a4baa0d17c13daa1841369191510be
jesusvalm97 <52518344+jesusvalm97@users.noreply.github.com>
2020-12-16 10:14:31 -0600

    remove about resource designer

--- src/MaterialDesignControls.Android/Resources/Resource.designer.cs
+++ src/MaterialDesignControls.Android/Resources/Resource.designer.cs
@@ -4118,31 +4118,28 @@ namespace MaterialDesignControls.Android
 			public static int hide_bottom_view_on_scroll_behavior = 2131492909;
 			
 			// aapt resource value: 0x7F0C002E
-			public static int library_name = 2131492910;
+			public static int mtrl_chip_close_icon_content_description = 2131492910;
 			
 			// aapt resource value: 0x7F0C002F
-			public static int mtrl_chip_close_icon_content_description = 2131492911;
+			public static int password_toggle_content_description = 2131492911;
 			
 			// aapt resource value: 0x7F0C0030
-			public static int password_toggle_content_description = 2131492912;
+			public static int path_password_eye = 2131492912;
 			
 			// aapt resource value: 0x7F0C0031
-			public static int path_password_eye = 2131492913;
+			public static int path_password_eye_mask_strike_through = 2131492913;
 			
 			// aapt resource value: 0x7F0C0032
-			public static int path_password_eye_mask_strike_through = 2131492914;
+			public static int path_password_eye_mask_visible = 2131492914;
 			
 			// aapt resource value: 0x7F0C0033
-			public static int path_password_eye_mask_visible = 2131492915;
+			public static int path_password_strike_through = 2131492915;
 			
 			// aapt resource value: 0x7F0C0034
-			public static int path_password_strike_through = 2131492916;
+			public static int search_menu_title = 2131492916;
 			
 			// aapt resource value: 0x7F0C0035
-			public static int search_menu_title = 2131492917;
-			
-			// aapt resource value: 0x7F0C0036
-			public static int status_bar_notification_info_overflow = 2131492918;
+			public static int status_bar_notification_info_overflow = 2131492917;
 			
 			static String()
 			{
